a5005a82eb805bb1382f2c4551a7596e0a2deebb
박다솜 <dev.somda@gmail.com>
2023-04-24 16:48:18 +0900

    ✨ feat : apply current location on map component

--- frontend/src/components/RealTime/map.tsx
+++ frontend/src/components/RealTime/map.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getLocation } from "./getLocation";
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 export default function Map() {
+  const [location, setLocation] = useState<
+    { latitude: number; longitude: number } | string
+  >("");
+
+  useEffect(() => {
+    getLocation().then((result) => {
+      setLocation(result);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (location && location !== "" && typeof location !== "string") {
+      const container = document.getElementById("map");
+      const options = {
+        center: new window.kakao.maps.LatLng(
+          location.latitude,
+          location.longitude
+        ),
+        level: 3,
+      };
+      const map = new window.kakao.maps.Map(container, options);
+    }
+  }, [location]);
+
   return (
-    <div>Map</div>
-  )
+    <div>
+      <p>카카오</p>
+      <div id="map" style={{ width: "500px", height: "400px" }} />
+    </div>
+  );
 }
--- frontend/src/pages/RealTime/index.tsx
+++ frontend/src/pages/RealTime/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Map from "@components/RealTime/map";
 
 export default function index() {
   return (
     <div>
       <p>실시간 페이지 입니다</p>
+      <Map />
     </div>
   );
 }
