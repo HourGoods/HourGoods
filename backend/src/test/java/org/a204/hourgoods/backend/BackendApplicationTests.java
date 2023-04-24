cdd29e0375d03f3d91a319e531cc8b8f565e02eb
Yeji Heo <yejixheo@gmail.com>
2023-04-24 10:42:47 +0900

    🎉 begin : initialize backend project

---
+++ backend/src/test/java/org/a204/hourgoods/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.a204.hourgoods.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
