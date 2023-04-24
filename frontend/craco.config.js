b8571863407b70a8eced7580c1a9c17aa2919f3f
박다솜 <dev.somda@gmail.com>
2023-04-24 15:36:11 +0900

    🎉 begin: create basic folder structure

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -8,13 +8,13 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
-      "@hooks": path.resolve(__dirname, "src/hooks"),
       "@pages": path.resolve(__dirname, "src/pages"),
       "@components": path.resolve(__dirname, "src/components"),
       "@assets": path.resolve(__dirname, "src/assets"),
-      "@stores": path.resolve(__dirname, "src/stores"),
+      "@recoils": path.resolve(__dirname, "src/recoils"),
       "@styles": path.resolve(__dirname, "src/styles"),
-      "@apis": path.resolve(__dirname, "src/APIs"),
+      "@apis": path.resolve(__dirname, "src/api"),
+      // "@hooks": path.resolve(__dirname, "src/hooks"),
     },
   },
 };
