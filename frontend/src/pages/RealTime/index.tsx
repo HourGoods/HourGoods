555e043f50c160dd6f754ad695e21b2840836373
박다솜 <dev.somda@gmail.com>
2023-04-25 09:58:38 +0900

    🎨 style: 폴더 구조 예시 파일 추가

--- frontend/src/pages/RealTime/index.tsx
+++ frontend/src/pages/RealTime/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Map from "@components/RealTime/map";
+import Map from "@components/RealTime/Map";
+import CardList from "@components/RealTime/CardList";
 
 export default function index() {
   return (
     <div>
       <p>실시간 페이지 입니다</p>
       <Map />
+      <CardList />
     </div>
   );
 }
