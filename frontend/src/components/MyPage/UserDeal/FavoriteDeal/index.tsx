5f5553e157f9791d422b4fc7f8fd7a8ee526134e
임길현 <limgilhyun@naver.com>
2023-04-25 13:42:23 +0900

    ✨ feat : create mypage

---
+++ frontend/src/components/MyPage/UserDeal/FavoriteDeal/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return <div>favoritedeal</div>;
+}
+
+export default index;
---
+++ frontend/src/components/MyPage/UserDeal/ParticipatedDeal/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return <div>participatedeal</div>;
+}
+
+export default index;
