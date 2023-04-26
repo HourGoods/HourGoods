87eac9a475d20e5fbbea1f13287fdaefb2fc596a
gimgyuyeon <8080claire@naver.com>
2023-04-26 16:24:18 +0900

    🚧 in progress: 글로벌 네브바 애니메이션 작업중

--- frontend/src/components/common/Nav/index.tsx
+++ frontend/src/components/common/Nav/index.tsx
@@ -1,17 +1,53 @@
-import React from "react";
-import { Bars3Icon, UserCircleIcon } from "@heroicons/react/24/solid";
+/*eslint-disable*/
+import React, { useState } from "react";
+import {
+  Bars3Icon,
+  UserCircleIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
 import "./index.scss";
 import logo from "@assets/logo.svg";
 
 export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header>
-      <div className="navbar-container">
-        <img src={logo} alt="로고" />
-        <p>실시간</p>
-        <p>탐색하기</p>
-        {/* <UserCircleIcon /> */}
+    <nav className="navbar">
+      <div className="web-navbar-wrapper">
+        <div className="web-navbar-logo-menu">
+          <img src={logo} alt="로고" />
+          <p>실시간</p>
+          <p>탐색하기</p>
+        </div>
+        <div className="web-navbar-profile">
+          <UserCircleIcon />
+        </div>
       </div>
-    </header>
+      <div className="mobile-navbar-wrapper">
+        <img src={logo} alt="로고" />
+        {isOpen ? "" : <Bars3Icon onClick={toggleMenu} />}
+        {isOpen && (
+          <div
+            className={`mobile-sidebar-wrapper ${
+              isOpen ? "" : "sidebar-closed"
+            }`}
+          >
+            <div className="mobile-nav-close-btn">
+              <XMarkIcon onClick={toggleMenu} />
+            </div>
+            <div className="mobile-sidebar-menu">
+              <p>실시간</p>
+              <p>탐색하기</p>
+              <p>마이페이지</p>
+              <p>로그인</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
   );
 }
