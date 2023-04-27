37f78f65154a7cf26d12f05950acccef10d361f5
gimgyuyeon <8080claire@naver.com>
2023-04-27 10:12:21 +0900

    ✨ feat: 로그인, 탐색하기 라우팅 추가

--- frontend/src/pages/Routers.tsx
+++ frontend/src/pages/Routers.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "@pages/Main";
 import RealTimePage from "@pages/RealTime";
-import MyPage from "@pages/MyPage"
+import SearchPage from "@pages/Search";
+import LoginPage from "@pages/Login";
+import MyPage from "@pages/MyPage";
 
 export default function Routers() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/realtime" element={<RealTimePage />} />
-      {/* User */}
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/mypage" element={<MyPage />} />
     </Routes>
   );
