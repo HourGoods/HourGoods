3c9982125ec91c378d14e9dce51e3e285104c2cb
Yeji Heo <yejixheo@gmail.com>
2023-04-27 13:52:05 +0900

    ⚡ update : modify schema annotation(name -> description)

--- backend/src/main/java/org/a204/hourgoods/domain/deal/request/ConcertDealListRequest.java
+++ backend/src/main/java/org/a204/hourgoods/domain/deal/request/ConcertDealListRequest.java
@@ -1,5 +1,6 @@
 package org.a204.hourgoods.domain.deal.request;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
@@ -8,24 +9,26 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
+@Setter
 @Schema(description = "콘서트별 거래 목록 조회 요청")
 public class ConcertDealListRequest {
 
 	@NotNull
 	@Positive
-	@Schema(name = "해당하는 콘서트 id")
+	@Schema(description = "해당하는 콘서트 id")
 	private Long concertId;
 
 	@NotNull
-	@Schema(name = "(페이지네이션용) 마지막 거래 id")
+	@Schema(description = "(페이지네이션용) 마지막 거래 id")
 	private Long lastDealId;
 
-	@NotNull
-	@Schema(name = "거래 종류, All/Auction/AuctionGame/Sharing/Trade")
+	@NotEmpty
+	@Schema(description = "거래 종류, All/Auction/AuctionGame/Sharing/Trade")
 	private String dealTypeName;
 }
