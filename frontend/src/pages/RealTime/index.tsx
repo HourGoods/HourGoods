42e5a2fdfde8252c25e3770b8472de3b7f6beaab
박다솜 <dev.somda@gmail.com>
2023-04-27 12:27:00 +0900

    ✨ feat : 실시간 페이지 반응형 ui 적용

--- frontend/src/pages/RealTime/index.tsx
+++ frontend/src/pages/RealTime/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Map from "@components/RealTime/Map";
 import CardList from "@components/RealTime/CardList";
+import "./index.scss";
 
 export default function index() {
   return (
-    <div>
-      <p>실시간 페이지 입니다</p>
+    <div className="realtime-page-container">
       <Map />
       <CardList />
     </div>
