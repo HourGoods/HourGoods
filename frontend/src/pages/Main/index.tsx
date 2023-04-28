4ab494518cbb613de43ee5715b0fcc06e858d01e
gimgyuyeon <8080claire@naver.com>
2023-04-28 10:10:10 +0900

    💄 design : 모달 css 작업

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -1,15 +1,17 @@
+// Main.tsx
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Modal from "@components/common/Modal";
 import Button from "@components/common/Button";
 import "./index.scss";
 
-export default function index() {
+export default function Main() {
   const [modalOpen, setModalOpen] = useState(false);
 
   const modalClickHandler = () => {
     setModalOpen(true);
   };
+
   return (
     <div>
       <div>
@@ -20,22 +22,29 @@ export default function index() {
             onClick={modalClickHandler}
             color="dark-blue"
           />
-          {modalOpen && <Modal setModalOpen={setModalOpen} />}
-          <Button text="purple 버튼" color="purple" />
-          <Button text="pink 버튼" color="pink" />
-          <Button text="yellow 버튼" color="yellow" />
         </div>
-        <h5>임시 가이드라인 정보</h5>
-        <p>mobile: red</p>
-        <p>tablet: green</p>
-        <p>desktop: blue</p>
-        <button
-          type="button"
-          style={{ margin: "20px", backgroundColor: "yellow" }}
-        >
-          <Link to="/create/deal">딜 생성하기</Link>
-        </button>
+        {modalOpen && (
+          <Modal setModalOpen={setModalOpen}>
+            <h1>여기에</h1>
+            <h3>원하는 내용을</h3>
+            <p>추가하시면 됩니다.</p>
+            <Button text="버튼도 추가 돼요" color="purple" />
+          </Modal>
+        )}
+        <Button text="purple 버튼" color="purple" />
+        <Button text="pink 버튼" color="pink" />
+        <Button text="yellow 버튼" color="yellow" />
       </div>
+      <h5>임시 가이드라인 정보</h5>
+      <p>mobile: red</p>
+      <p>tablet: green</p>
+      <p>desktop: blue</p>
+      <button
+        type="button"
+        style={{ margin: "20px", backgroundColor: "yellow" }}
+      >
+        <Link to="/create/deal">딜 생성하기</Link>
+      </button>
     </div>
   );
 }
