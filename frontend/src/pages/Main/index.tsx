4f3fabd0c259954ff6686c2fd38331963e2c52ce
gimgyuyeon <8080claire@naver.com>
2023-04-28 13:36:13 +0900

    ✨ feat : 거래 deal button 생성

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -11,28 +11,52 @@ export default function Main() {
     setModalOpen(true);
   };
 
+  // const baseUrl = "https://k8a204.p.ssafy.io";
+  const baseUrl = "http://localhost:3000";
+  const loginUrl = `${baseUrl}/oauth2/authorization/kakao`;
+
   return (
     <div>
-      <div>
+      <div className="mainpage-button-container">
         {/* 모달 버튼은 사라질 겁니다. */}
-        <div className="mainpage-test-modal-button">
-          <Button
-            text="모달테스트"
-            onClick={modalClickHandler}
-            color="dark-blue"
-          />
-        </div>
+        <Button onClick={modalClickHandler} color="dark-blue">
+          모달테스트
+        </Button>
         {modalOpen && (
           <Modal setModalOpen={setModalOpen}>
             <h1>여기에</h1>
             <h3>원하는 내용을</h3>
             <p>추가하시면 됩니다.</p>
-            <Button text="버튼도 추가 돼요" color="purple" />
+            <Button color="purple">버튼도 추가됩니다.</Button>
           </Modal>
         )}
-        <Button text="purple 버튼" color="purple" />
-        <Button text="pink 버튼" color="pink" />
-        <Button text="yellow 버튼" color="yellow" />
+        <Button color="purple">purple 버튼</Button>
+        <Button color="pink">pink 버튼</Button>
+        <Button color="yellow">yellow 버튼</Button>
+        <Button color="indigo" size="small">
+          예
+        </Button>
+        <Button color="white" size="small">
+          아니오
+        </Button>
+        <Button color="shotpink" size="deal">
+          전체보기
+        </Button>
+        <Button color="spurple" size="deal">
+          Hour경매
+        </Button>
+        <Button color="sindigo" size="deal">
+          경매
+        </Button>
+        <Button color="syellow" size="deal">
+          나눔
+        </Button>
+        <Button color="spink" size="deal">
+          거래
+        </Button>
+        <a href={loginUrl}>
+          <Button color="kakao">카카오 로그인</Button>
+        </a>
       </div>
       <h5>임시 가이드라인 정보</h5>
       <p>mobile: red</p>
