e03b627e3ff4d0bb9b84d0621f77fa12e758d3ca
gimgyuyeon <8080claire@naver.com>
2023-04-28 10:10:10 +0900

    💄 design : 모달 css 작업

--- frontend/src/components/common/Modal/index.tsx
+++ frontend/src/components/common/Modal/index.tsx
@@ -1,29 +1,40 @@
+// Modal.tsx
 import React, { useRef } from "react";
 import useModalRef from "@hooks/useModalRef";
 import "./index.scss";
 import { XCircleIcon } from "@heroicons/react/24/solid";
-import blueCloud from "@assets/blueCloud.svg";
-import yellowCloud from "@assets/yellowCloud.svg";
+import pinkCloud from "@assets/pinkCloud.svg";
+import purpleCloud from "@assets/purpleCloud.svg";
 
 interface IModalProps {
   setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  children: React.ReactNode;
 }
 
-export default function index({ setModalOpen }: IModalProps) {
+export default function Modal({ setModalOpen, children }: IModalProps) {
   const modalRef = useRef<HTMLDivElement>(null);
   useModalRef(modalRef, () => setModalOpen(false));
 
   const closeModal = () => {
     setModalOpen(false);
   };
+
   return (
-    <div className="container" ref={modalRef}>
-      <img src={blueCloud} alt="" />
-      <button type="button" className="close" onClick={closeModal}>
-        <XCircleIcon />
-      </button>
-      <p>모달창입니다.</p>
-      <img src={yellowCloud} alt="" />
+    <div className="common-modal-container">
+      <div className="common-modal-overlay">
+        <div className="common-modal-box" ref={modalRef}>
+          <img src={pinkCloud} alt="" className="upper-cloud-img" />
+          <button
+            type="button"
+            className="common-modal-close-button"
+            onClick={closeModal}
+          >
+            <XCircleIcon />
+          </button>
+          {children}
+          <img src={purpleCloud} alt="" className="bottom-cloud-img" />
+        </div>
+      </div>
     </div>
   );
 }
