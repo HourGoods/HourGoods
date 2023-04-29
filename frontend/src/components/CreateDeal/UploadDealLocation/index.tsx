b75696fd6199a5089b393901630ccdebf8c5666e
Dasom Park <dev.somda@gmail.com>
2023-04-29 18:23:04 +0900

    ✨ feat : open deal page UI 적용

--- frontend/src/components/CreateDeal/UploadDealLocation/index.tsx
+++ frontend/src/components/CreateDeal/UploadDealLocation/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useEffect } from "react";
 
 declare global {
@@ -38,13 +39,40 @@ export default function index() {
     window.kakao.maps.event.addListener(marker, "dragend", handleMarkerDragend);
 
     marker.setDraggable(true);
+
+    // 마커 위에 infoWindow
+
+    const iwContent =
+        '<div style="padding:5px;">거래 장소를 지정해주세요!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+      iwPosition = new window.kakao.maps.LatLng(
+        37.511806050815686,
+        127.07376866170583
+      ); //인포윈도우 표시 위치입니다
+
+    // 인포윈도우를 생성합니다
+    const infowindow = new window.kakao.maps.InfoWindow({
+      position: iwPosition,
+      content: iwContent,
+    });
+
+    // 마커에 마우스오버 이벤트를 등록합니다
+    window.kakao.maps.event.addListener(marker, "mouseover", function () {
+      // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+      infowindow.open(map, marker);
+    });
+
+    // 마커에 마우스아웃 이벤트를 등록합니다
+    window.kakao.maps.event.addListener(marker, "mouseout", function () {
+      // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+      infowindow.close();
+    });
   }, []);
 
   return (
     <div className="create-deal-location-component-container">
-      <p>주소</p>
-      <input type="address" />
+      <p>거래 장소</p>
       <div id="map" />
+      <input type="address" placeholder="상세 위치" />
     </div>
   );
 }
