16cb97febb8be8a530fe06cb2c5e5193b02b9a49
Dasom Park <dev.somda@gmail.com>
2023-04-29 15:56:37 +0900

    ✨ feat : 임시 바로가기 버튼 영역 추가

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -1,4 +1,3 @@
-// Main.tsx
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Modal from "@components/common/Modal";
@@ -18,12 +17,57 @@ export default function Main() {
 
   return (
     <div>
-      <div>
-        <h5>임시 가이드라인 정보</h5>
-        <p>mobile: red</p>
-        <p>tablet: green</p>
-        <p>desktop: blue</p>
+      <div className="mainpage-button-container">
+        {/* 모달 버튼은 사라질 겁니다. */}
+        <Button onClick={modalClickHandler} color="dark-blue">
+          모달테스트
+        </Button>
+        {modalOpen && (
+          <Modal setModalOpen={setModalOpen}>
+            <h1>여기에</h1>
+            <h3>원하는 내용을</h3>
+            <p>추가하시면 됩니다.</p>
+            <Button color="purple">버튼도 추가됩니다.</Button>
+          </Modal>
+        )}
+        <Button color="purple">purple 버튼</Button>
+        <Button color="pink">pink 버튼</Button>
+        <Button color="yellow">yellow 버튼</Button>
+        <Button color="indigo" size="small">
+          예
+        </Button>
+        <Button color="white" size="small">
+          아니오
+        </Button>
+        <Button color="shotpink" size="deal">
+          전체보기
+        </Button>
+        <Button color="spurple" size="deal">
+          Hour경매
+        </Button>
+        <Button color="sindigo" size="deal">
+          경매
+        </Button>
+        <Button color="syellow" size="deal">
+          나눔
+        </Button>
+        <Button color="spink" size="deal">
+          거래
+        </Button>
+        <a href={loginUrl}>
+          <Button color="kakao">카카오 로그인</Button>
+        </a>
       </div>
+      <div className="temp-direction-div">
+        <p>임시 바로가기 모음</p>
+        <Link to="/create/deal">
+          <button type="button">Deal 생성 바로가기</button>
+        </Link>
+      </div>
+      <h5>임시 가이드라인 정보</h5>
+      <p>mobile: red</p>
+      <p>tablet: green</p>
+      <p>desktop: blue</p>
     </div>
   );
 }
