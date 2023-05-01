9a57925b5718364fb1a60d4491eb6ad0c08a075d
dhkim <xswaqz@naver.com>
2023-05-01 17:48:41 +0900

    ⚡ update : Concert 레파지토리에 검색 추가

--- backend/src/main/java/org/a204/hourgoods/domain/concert/repository/ConcertRepository.java
+++ backend/src/main/java/org/a204/hourgoods/domain/concert/repository/ConcertRepository.java
@@ -1,7 +1,10 @@
 package org.a204.hourgoods.domain.concert.repository;
 
+import java.util.List;
+
 import org.a204.hourgoods.domain.concert.entity.Concert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConcertRepository extends JpaRepository<Concert, Long> {
+	List<Concert> findAllByTitleContaining(String keyword);
 }
