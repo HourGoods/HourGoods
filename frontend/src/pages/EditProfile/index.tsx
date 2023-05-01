4ce6e5cdd258a325d86614e40792942720ec3b78
gimgyuyeon <8080claire@naver.com>
2023-05-02 00:34:51 +0900

    🚧 in progress : 소셜 로그인 진행중

--- frontend/src/pages/EditProfile/index.tsx
+++ frontend/src/pages/EditProfile/index.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+/* eslint-disable  */
+
+import { memberAPI } from "@api/apis";
+import { UserStateAtom } from "@recoils/user/Atom";
+import React from "react";
+import { useRecoilState } from "recoil";
 
 export default function index() {
+  const [userInfo, setUserInfo] = useRecoilState(UserStateAtom);
+  const nickname = "이지은";
+  const imgUrl =
+    "https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/06/28/612b6ff6-5081-4b54-a22c-38d716229c41.jpg";
+
+  const clickHandler = () => {
+    setUserInfo({
+      nickname: nickname,
+      imgUrl: imgUrl,
+    });
+    memberAPI.signup(userInfo);
+  };
+
   return (
-    <div>프로필 수정페이지입니다.</div>
-  )
+    <>
+      <div>수정페이지</div>
+      <button onClick={clickHandler}>회원가입 되나요?</button>
+    </>
+  );
 }
