e32a9629106a6b869301fc62b26dda930dbe2e49
johan kong <johankkong@gmail.com>
2023-05-01 14:46:55 +0900

    ✨ feat : add deal deletion feature

--- backend/src/main/java/org/a204/hourgoods/domain/deal/contorller/DealController.java
+++ backend/src/main/java/org/a204/hourgoods/domain/deal/contorller/DealController.java
@@ -15,13 +15,16 @@ import org.a204.hourgoods.domain.deal.request.ConcertDealListRequest;
 import org.a204.hourgoods.domain.deal.request.DealCreateRequest;
 import org.a204.hourgoods.domain.deal.response.ConcertDealListResponse;
 import org.a204.hourgoods.domain.deal.response.DealCreateResponse;
+import org.a204.hourgoods.domain.deal.response.DealDeletionResponse;
 import org.a204.hourgoods.domain.deal.response.DealDetailResponse;
 import org.a204.hourgoods.domain.deal.service.DealService;
 import org.a204.hourgoods.domain.member.entity.Member;
 import org.a204.hourgoods.domain.member.entity.MemberDetails;
 import org.a204.hourgoods.global.common.BaseResponse;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -72,7 +75,7 @@ public class DealController {
 	 */
 	@Operation(summary = "거래 생성 API", description = "경매, 게임경매, 거래, 나눔 모두 하나의 API로 생성, 종류에 따라 필요한 request 다름")
 	@ApiResponse(responseCode = "200", description = "생성 완료", content = @Content(schema = @Schema(implementation = DealCreateResponse.class)))
-	@ApiResponse(responseCode = "400", description = "1. M300 해당 사용자ID 조회 실패 \t\n 2. M400 요청 사용자와 생성 사용자 ID 뷸일치 ")
+	@ApiResponse(responseCode = "400", description = "1. M300 해당 사용자ID 조회 실패 \t\n 2. M400 요청 사용자와 생성 사용자 ID 불일치")
 	@ApiResponse(responseCode = "404", description = "1. C100 해당 콘서트ID 조회 실패")
 	@PostMapping("/create")
 	public BaseResponse<DealCreateResponse> createDeal(@AuthenticationPrincipal MemberDetails memberDetails, @RequestBody @Valid DealCreateRequest dealCreateRequest) {
@@ -80,5 +83,17 @@ public class DealController {
 		if (member.getId() != dealCreateRequest.getMemberId()) throw new MemberMissMatchException();
 		DealCreateResponse dealCreateResponse = dealService.createDeal(dealCreateRequest);
 		return new BaseResponse<>(dealCreateResponse);
+	}
+
+	@Operation(summary = "거래 삭제 API", description = "거래 삭제 API, 거래를 생성한 사람만 삭제가 가능, pathVariable로 거래ID 요청")
+	@ApiResponse(responseCode = "200", description = "거래 삭제 완료", content = @Content(schema = @Schema(implementation = DealDeletionResponse.class)))
+	@ApiResponse(responseCode = "400", description = "1. M400 요청 사용자와 생성 사용자 ID 불일치")
+	@ApiResponse(responseCode = "404", description = "1. D200 해당 거래ID 조회 실패")
+	@DeleteMapping("/{dealId}")
+	public BaseResponse<DealDeletionResponse> deleteDeal(@AuthenticationPrincipal MemberDetails memberDetails, @PathVariable Long dealId) {
+		Member member = memberDetails.getMember();
+		DealDeletionResponse response = DealDeletionResponse.builder()
+			.isSuccess(dealService.deleteDeal(member.getId(), dealId)).build();
+		return new BaseResponse<>(response);
 	}
 }
