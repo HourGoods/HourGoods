5604237e2093bfd749c61ce290abd3b90727d9a9
hiflo <xswaqz@naver.com>
2023-05-02 21:54:22 +0900

    🐛 fix : Concert API request Type 오류 수정

--- backend/src/main/java/org/a204/hourgoods/domain/concert/controller/ConcertController.java
+++ backend/src/main/java/org/a204/hourgoods/domain/concert/controller/ConcertController.java
@@ -2,6 +2,7 @@ package org.a204.hourgoods.domain.concert.controller;
 
 import javax.validation.Valid;
 
+import org.a204.hourgoods.domain.concert.request.ConcertIdRequest;
 import org.a204.hourgoods.domain.concert.request.TodayConcertRequest;
 import org.a204.hourgoods.domain.concert.response.ConcertIdResponse;
 import org.a204.hourgoods.domain.concert.response.ConcertInfoResponse;
@@ -12,6 +13,7 @@ import org.a204.hourgoods.domain.concert.service.KopisService;
 import org.a204.hourgoods.global.common.BaseResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,15 +38,15 @@ public class ConcertController {
 	@Operation(description = "kopisConcertId와 일치하는 공연의 id를 조회한다.(없으면 생성해서 반환)", summary = "공연 아이디 조회 API")
 	@ApiResponse(responseCode = "201", description = "등록 성공", content = @Content(schema = @Schema(implementation = ConcertIdResponse.class)))
 	@PostMapping()
-	public BaseResponse<ConcertIdResponse> getConcertId(@RequestParam(name = "kopisConcertId") String kopisConcertId) {
-		ConcertIdResponse response = concertService.getConcertId(kopisConcertId);
+	public BaseResponse<ConcertIdResponse> getConcertId(@RequestBody ConcertIdRequest concertIdRequest) {
+		ConcertIdResponse response = concertService.getConcertId(concertIdRequest);
 		return new BaseResponse<>(response);
 	}
 
 	@Operation(description = "공연 id와 일치하는 공연 상세 정보를 조회한다.", summary = "공연 상세 정보 조회 API")
 	@ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = ConcertInfoResponse.class)))
 	@GetMapping()
-	public BaseResponse<ConcertInfoResponse> getConcertList(@RequestParam(name = "concertId") Long concertId) {
+	public BaseResponse<ConcertInfoResponse> getConcertDetail(@RequestParam(name = "concertId") Long concertId) {
 		ConcertInfoResponse response = concertService.getConcertDetail(concertId);
 		return new BaseResponse<>(response);
 	}
@@ -60,7 +62,7 @@ public class ConcertController {
 	@Operation(description = "오늘 시작하는 사용자와 가까운 순으로 정렬된 공연의 정보 리스트를 조회한다.", summary = "오늘의 사용자 주변 공연 정보 목록 조회 API")
 	@ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = TodayConcertListResponse.class)))
 	@GetMapping("/today")
-	public BaseResponse<TodayConcertListResponse> getConcertList(@Valid TodayConcertRequest todayConcertRequest) {
+	public BaseResponse<TodayConcertListResponse> getTodayConcertList(@Valid TodayConcertRequest todayConcertRequest) {
 		TodayConcertListResponse response = concertService.getTodayConcertList(todayConcertRequest);
 		return new BaseResponse<>(response);
 	}
