a54d91e25ef51a4cd1b00742a472b8a10480d513
Dasom Park <dev.somda@gmail.com>
2023-05-07 00:22:43 +0900

    ✨ feat : concert별 deal검색

--- frontend/src/components/ConcertDeal/SearchTab/index.tsx
+++ frontend/src/components/ConcertDeal/SearchTab/index.tsx
@@ -1,65 +1,104 @@
 import React, { useState } from "react";
+import { concertAPI } from "@api/apis";
+import { useRecoilValue } from "recoil";
+import { UserStateAtom } from "@recoils/user/Atom";
 import Button from "@components/common/Button";
 import SearchBar from "@components/common/SearchBar";
 
-export default function index() {
+export default function index(props: any) {
+  const { concertId, setConcertDealList } = props;
   const [activeDealType, setActiveDealType] = useState({
-    all: true,
-    trade: false,
-    sharing: false,
-    auction: false,
-    hourAuction: false,
+    All: true,
+    Trade: false,
+    Sharing: false,
+    Auction: false,
+    HourAuction: false,
   });
+  const [searchInput, setSearchInput] = useState("");
+  const userInfo = useRecoilValue(UserStateAtom);
 
   const activationHandler = (type: string) => {
     setActiveDealType((prev: any) => ({
       ...prev,
-      all: type === "all",
-      trade: type === "trade",
-      sharing: type === "sharing",
-      auction: type === "auction",
-      hourAuction: type === "hourAuction",
+      All: type === "All",
+      Trade: type === "Trade",
+      Sharing: type === "Sharing",
+      Auction: type === "Auction",
+      HourAuction: type === "HourAuction",
     }));
+
+    // api
+    const result = concertAPI.getConcertDealList(
+      concertId,
+      -1,
+      type,
+      "",
+      userInfo.nickname
+    );
+    result.then((res) => {
+      console.log("콘서트별 딜 정보", res);
+      setConcertDealList(res.data.result.dealInfoList);
+    });
   };
+
+  const searchHanler = () => {
+    // api
+    const result = concertAPI.getConcertDealList(
+      concertId,
+      -1,
+      "All",
+      searchInput,
+      userInfo.nickname
+    );
+    result.then((res) => {
+      console.log("콘서트별 딜 정보", res);
+      setConcertDealList(res.data.result.dealInfoList);
+    });
+  };
+
   return (
     <div>
       <div className="deal-type-buttons-container">
         <Button
           color="All"
           size="deal"
-          isActive={activeDealType.all}
-          onClick={() => activationHandler("all")}
+          isActive={activeDealType.All}
+          onClick={() => activationHandler("All")}
         />
         <Button
           color="Trade"
           size="deal"
-          isActive={activeDealType.trade}
-          onClick={() => activationHandler("trade")}
+          isActive={activeDealType.Trade}
+          onClick={() => activationHandler("Trade")}
         />
 
         <Button
           color="Sharing"
           size="deal"
-          isActive={activeDealType.sharing}
-          onClick={() => activationHandler("sharing")}
+          isActive={activeDealType.Sharing}
+          onClick={() => activationHandler("Sharing")}
         />
 
         <Button
           color="Auction"
           size="deal"
-          isActive={activeDealType.auction}
-          onClick={() => activationHandler("auction")}
+          isActive={activeDealType.Auction}
+          onClick={() => activationHandler("Auction")}
         />
 
         <Button
           color="HourAuction"
           size="deal"
-          isActive={activeDealType.hourAuction}
-          onClick={() => activationHandler("hourAuction")}
+          isActive={activeDealType.HourAuction}
+          onClick={() => activationHandler("HourAuction")}
         />
       </div>
 
-      <SearchBar />
+      <SearchBar
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+        onSubmit={searchHanler}
+      />
     </div>
   );
 }
