4e09a4049e55d7fa47edee4a2daa4f593d46d788
johan kong <johankkong@gmail.com>
2023-05-08 10:25:41 +0900

    ⚡ update : add bidding entity

---
+++ backend/src/main/java/org/a204/hourgoods/domain/bidding/entity/Bidding.java
@@ -0,0 +1,42 @@
+package org.a204.hourgoods.domain.bidding.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.a204.hourgoods.domain.deal.entity.Deal;
+import org.a204.hourgoods.domain.member.entity.Member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "bidding")
+public class Bidding {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	@Column(name = "bid_amount")
+	private Integer bidAmount;
+
+	@Column(name = "is_winner")
+	private Boolean isWinner;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "purchaser_id")
+	private Member bidder;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "deal_id")
+	private Deal deal;
+}
