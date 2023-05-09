9c2b1b3957e4958a3b9c48ee3cc3d45c49de454a
gimgyuyeon <8080claire@naver.com>
2023-05-09 17:46:33 +0900

    ✨ feat : JOIN, EXIT 기능추가

--- frontend/src/components/Auction/ChattingBox/index.tsx
+++ frontend/src/components/Auction/ChattingBox/index.tsx
@@ -1,17 +1,22 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/no-array-index-key */
-import React from "react";
+import React, { useEffect } from "react";
 import { UserStateAtom } from "@recoils/user/Atom";
 import { useRecoilValue } from "recoil";
-import { ChatMessage } from "..";
+import { ChatMessage, InoutMessage } from "..";
 
 interface Props {
   msgList: ChatMessage[];
+  inoutMsgList: InoutMessage[];
 }
 
-export default function index({ msgList }: Props) {
+export default function index({ msgList, inoutMsgList }: Props) {
   const userInfo = useRecoilValue(UserStateAtom);
   const userName = userInfo.nickname;
+
+  useEffect(() => {
+    console.log("inoutmessage list", inoutMsgList);
+  }, [inoutMsgList]);
 
   return (
     <div className="chattingbox-all-container">
@@ -35,39 +40,19 @@ export default function index({ msgList }: Props) {
             </div>
           );
         })}
+        {inoutMsgList.map((message: InoutMessage, index: number) => {
+          const join = message.messageType === "JOIN";
+          return (
+            <div key={index} className="in-out-message">
+              {join ? (
+                <p>{message.nickname}님이 참여하였습니다.</p>
+              ) : (
+                <p>{message.nickname}님이 퇴장하였습니다.</p>
+              )}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 }
-
-// const messageClassName =
-//   message.nickname === userName ? "its-me-chat" : "not-me-chat";
-// return (
-//   <div key={index} className={messageClassName}>
-//     {message.nickname !== userName && (
-//       <img src={message.imageUrl} alt="프로필사진" />
-//     )}
-//     <div className="not-me-chat-message">
-//       <p className="not-me-chat-name">{message.nickname}</p>
-//       <p className="not-me-chat-message">{message.content}</p>
-//     </div>
-//   </div>
-// );
-
-// <div className="not-me-chat">
-//         <UserCircleIcon />
-//         <div className="not-me-chat-message">
-//           <p className="not-me-chat-name">{nickname}</p>
-//           <p className="not-me-chat-message">{messages}</p>
-//         </div>
-//       </div>
-//       <div className="its-me-chat">
-//         <p className="its-me-chat-box">내가 보낸 메세지</p>
-//       </div>
-//       <div className="not-me-chat">
-//         <UserCircleIcon />
-//         <div className="not-me-chat-message">
-//           <p className="not-me-chat-name">아이유사랑해</p>
-//           <p className="not-me-chat-message">남이 보낸 메세지</p>
-//         </div>
-//       </div>
