f878216027efc808d2e732d74f78736479ba4f12
dhkim <xswaqz@naver.com>
2023-05-11 16:09:29 +0900

    ♻ refactor : 1:1 거래 시 실시간 위치 확인 API 응답 설명 코드 수정

--- backend/src/main/java/org/a204/hourgoods/domain/deal/contorller/TradeController.java
+++ backend/src/main/java/org/a204/hourgoods/domain/deal/contorller/TradeController.java
@@ -20,6 +20,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,7 +34,13 @@ public class TradeController {
 	private final TradeService tradeService;
 
 	@Operation(summary = "거래자 실시간 위치 확인 API", description = "거래자의 위치정보 확인을 위해 소켓 연결 성공")
-	@ApiResponse(responseCode = "200", description = "위치 정보 전송 성공", content = @Content(schema = @Schema(implementation = CreateTradeLocationResponse.class)))
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "위치 정보 전송 성공", content = @Content(schema = @Schema(implementation = CreateTradeLocationResponse.class))),
+		@ApiResponse(responseCode = "400", description = "1. D200 해당 id에 해당하는 거래가 없습니다.\n"
+			+ "2. M500 판매자 닉네임과 일치하는 사용자가 없습니다.\n"
+			+ "3. M501 판매자와 거래 등록자의 id가 일치하지 않습니다.\n"
+			+ "4. M600 구매자 닉네임과 일치하는 사용자가 없습니다.")
+	})
 	@PostMapping
 	public BaseResponse<CreateTradeLocationResponse> createTradeLocation(
 		@Valid @RequestBody CreateTradeLocationRequest request) {
