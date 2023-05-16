bf80acd463924192c2691a2bc3aea2f47d53e796
Dasom Park <dev.somda@gmail.com>
2023-05-16 18:57:15 +0900

    🚧 in progress : map 작업 중

--- frontend/src/components/MeatingDeal/MeetingSocket/Map/index.tsx
+++ frontend/src/components/MeatingDeal/MeetingSocket/Map/index.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
-import getCurrentLocation from "@utils/getCurrentLocation";
-import watchCurrentLocation from "@utils/watchCurrentLocation";
+// import getCurrentLocation from "@utils/getCurrentLocation";
+// import watchCurrentLocation from "@utils/watchCurrentLocation";
 import meMarker from "@assets/userLocPoint.svg";
 import youMarker from "@assets/otherUserLocPoint.svg";
 import { useLocation } from "react-router-dom";
-import { IMapProps } from "../index";
+import { MeetingDealInfo } from "../index";
 
 interface IMapPropsType {
-  mapPropsState: IMapProps;
+  meetingInfo: MeetingDealInfo;
   userName: string;
   clientRef: any;
   tradeLocId: string;
+  dealId: any;
 }
 
 declare global {
@@ -20,9 +21,7 @@ declare global {
 }
 
 export default function Map(props: IMapPropsType) {
-  const { mapPropsState, userName, clientRef, tradeLocId } = props;
-  const location = useLocation();
-  const dealId = location.state.dealid;
+  const { meetingInfo, userName, clientRef, tradeLocId, dealId } = props;
   const [map, setMap] = useState<any>(null);
 
   const [myLocation, setMyLocation] = useState({});
@@ -45,96 +44,45 @@ export default function Map(props: IMapPropsType) {
     clientRef.current?.publish({ destination, body });
   };
 
-  // 최초의 지도 그리기
+
   useEffect(() => {
-    console.log("tradeLocId", tradeLocId);
-    // 임시 map 위치
-    const container = document.getElementById("map");
-    const options = {
-      center: new window.kakao.maps.LatLng(37.5665, 126.978), // 기본 위치
-      level: 4,
+    // 위치 정보를 가져오는 함수
+    const getLocation = () => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { longitude, latitude } = position.coords;
+          setMyLocation(position.coords);
+          sendMyLocation(longitude, latitude);
+        },
+        (error) => {
+          console.error("Failed to get current location:", error);
+        }
+      );
     };
-    const map = new window.kakao.maps.Map(container, options);
-    setMap(map);
-    // 마운팅이 된 상태라는 뜻
-    setFlag(1);
-  }, []);
 
-  // 최초의 위치
-  useEffect(() => {
-    if (map && flag === 1) {
-      console.log("여기는 맵은 있다");
-      // 내 위치
-      getCurrentLocation().then((res) => {
-        if (typeof res === "object" && res !== null) {
-          setMyLocation(res);
-          map.setCenter(
-            new window.kakao.maps.LatLng(res.latitude, res.longitude)
-          );
-          sendMyLocation(res.latitude, res.longitude);
-
-          // 상대 위치 그리기
-          // marker 생성
-          const markerImage = new window.kakao.maps.MarkerImage(
-            youMarker, // 마커 이미지 URL
-            new window.kakao.maps.Size(40, 40), // 마커 이미지 크기
-            {
-              offset: new window.kakao.maps.Point(55, 55),
-              alt: "현재 위치",
-            }
-          );
-          const newMarker = new window.kakao.maps.Marker({
-            position: new window.kakao.maps.LatLng(
-              mapPropsState.otherLatitude,
-              mapPropsState.otherLongitude
-            ),
-            image: markerImage,
-          });
-
-          newMarker.setMap(map);
-          setYourMarker(newMarker);
-
-          // flag 2로 변경(너랑 내 최초 위치는 그려진 상태)
-          setFlag(2);
-          console.log("플래그 변경ㅋ");
-        }
-      });
-    }
-  }, [map, flag]);
-
-  useEffect(() => {
-    let watcher: number | undefined;
-    if (map && flag === 2 && navigator.geolocation) {
-      console.log("갱신 준비 완");
-
-      watcher = watchCurrentLocation(map, (res: any) => {
-        if (typeof res === "object" && res !== null) {
-          sendMyLocation(res.latitude, res.longitude);
-        }
-      });
-    }
-  }, [map, flag, myLocation]);
-
-  // map이 생성되었는지 여부를 확인하고, 갱신될때마다 위치 변경 시작!
-  // 상대방
-  useEffect(() => {
-    if (map && flag) {
-      console.log("판매자 이동");
-      if (yourMarker) {
-        console.log(
-          mapPropsState.otherLatitude,
-          mapPropsState.otherLongitude,
-          "있다 야"
-        );
-
-        // yourMarker.setMap(null);
+    // 위치 정보 변경 시 sendMyLocation 함수 자동 호출
+    const watchLocation = navigator.geolocation.watchPosition(
+      (position) => {
+        const { longitude, latitude } = position.coords;
+        setMyLocation(position.coords);
+        sendMyLocation(longitude, latitude);
+      },
+      (error) => {
+        console.error("Failed to watch location:", error);
       }
-    }
-  }, [mapPropsState.otherLatitude, mapPropsState.otherLongitude]);
+    );
+
+    getLocation();
+
+    // 컴포넌트가 언마운트될 때 위치 정보 변경 감시를 정리합니다.
+    return () => {
+      navigator.geolocation.clearWatch(watchLocation);
+    };
+  }, []);
 
   return (
     <div>
-      <p>{mapPropsState.distance}m 떨어져있습니다</p>
+      <p>{meetingInfo.distance}m 떨어져있습니다</p>
       {/* 크기는 원하는대로 변경 가능! */}
       <div
         id="map"
--- frontend/src/components/MeatingDeal/MeetingSocket/index.tsx
+++ frontend/src/components/MeatingDeal/MeetingSocket/index.tsx
@@ -7,13 +7,6 @@ import SockJS from "sockjs-client";
 import Map from "./Map";
 import Loading from "./Loading";
 
-export interface IMapProps {
-  otherNickname: string; // 판매자이름
-  otherLongitude: number; // 판매자경도
-  otherLatitude: number; // 판매자위도
-  distance: number; // 판매자와 구매자의 거리
-}
-
 // 소켓통신으로 수신받은 결과값을 저장한 인스턴스
 export interface MeetingDealInfo {
   otherNickname: string; // 판매자이름
@@ -32,17 +25,14 @@ export default function index({ tradeLocId }: Props) {
   const userInfo = useRecoilValue(UserStateAtom);
   const userName = userInfo.nickname; // 내이름
 
-  // 다솜: Map을 위한 props 입니다
-  const [mapPropsState, setMapPropsState] = useState<IMapProps>({
+  // --------------------- 소켓 통신하기 -------------------------
+  const clientRef = useRef<Client>();
+  const [meetingInfo, setMeetingInfo] = useState<MeetingDealInfo>({
     otherNickname: "", // 판매자이름
     otherLongitude: 0, // 판매자경도
     otherLatitude: 0, // 판매자위도
     distance: 0, // 판매자와 구매자의 거리
   });
-
-  // --------------------- 소켓 통신하기 -------------------------
-  const clientRef = useRef<Client>();
-  const [meetingInfo, setMeetingInfo] = useState<MeetingDealInfo>();
 
   useEffect(() => {
     if (!clientRef.current) connect();
@@ -91,10 +81,11 @@ export default function index({ tradeLocId }: Props) {
       <Loading />
       {tradeLocId && (
         <Map
-          mapPropsState={mapPropsState}
+          meetingInfo={meetingInfo}
           userName={userName}
           clientRef={clientRef}
           tradeLocId={tradeLocId}
+          dealId={dealId}
         />
       )}
     </div>
