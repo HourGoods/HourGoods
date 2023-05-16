990d82661c939c763454e76c2a0df43925281793
gimgyuyeon <8080claire@naver.com>
2023-05-16 09:13:58 +0900

    🐛 fix : 스크롤 중 scrollToBottom 함수 방지

--- frontend/src/components/Auction/ChattingBox/index.tsx
+++ frontend/src/components/Auction/ChattingBox/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable consistent-return */
 /* eslint-disable react/no-array-index-key */
 import React, { useEffect, useRef, useState } from "react";
@@ -13,9 +14,11 @@ interface Props {
 
 export default function index({ msgList, inoutMsgList }: Props) {
   const chatMsgListRef = useRef<HTMLDivElement | null>(null);
+  const chatBottomRef = useRef<HTMLDivElement | null>(null);
   const userInfo = useRecoilValue(UserStateAtom);
   const userName = userInfo.nickname;
-  const [isScrollAtBottom, setIsScrollAtBottom] = useState<boolean>(false);
+  const [isScrollAtBottom, setIsScrollAtBottom] = useState<boolean>(true);
+  const [isNewMessage, setIsNewMessage] = useState<boolean>(false);
 
   useEffect(() => {
     if (chatMsgListRef.current) {
@@ -29,64 +32,79 @@ export default function index({ msgList, inoutMsgList }: Props) {
   }, [chatMsgListRef.current]);
 
   useEffect(() => {
-    // if (!isScrollAtBottom) {
-    //   scrollToBottom(chatMsgListRef.current);
+    console.log("isScrollAtBottom", isScrollAtBottom);
+    if (isScrollAtBottom) {
+      scrollToBottom(chatBottomRef.current);
+    }
+    // else {
+    //   setIsNewMessage(true);
     // }
-    scrollToBottom(chatMsgListRef.current);
-  }, [msgList, inoutMsgList]);
+  }, [msgList, inoutMsgList, isScrollAtBottom]);
 
   const handleScroll = () => {
     const element = chatMsgListRef?.current;
     if (element) {
       const { scrollTop, scrollHeight, clientHeight } = element;
-
       const isScrolledToBottom =
-        Math.abs(scrollTop + clientHeight - scrollHeight) <= 1;
+        Math.abs(scrollTop + clientHeight - scrollHeight) <= 50;
       setIsScrollAtBottom(isScrolledToBottom);
-      console.log(isScrolledToBottom);
     }
   };
 
+  // const handleButtonClick = () => {
+  //   setIsNewMessage(false);
+  //   scrollToBottom(chatBottomRef.current);
+  // };
+
   return (
     <div ref={chatMsgListRef} className="chattingbox-all-container">
-      {/* <div ref={chatMsgListRef} className="private-chatroom-content-container"> */}
-      {msgList.map((message: ChatMessage, index: number) => {
-        const isMe = message.nickname === userName;
-        const messageClassName = isMe ? "its-me-chat" : "not-me-chat";
-        const join = message.messageType === "JOIN";
-
-        return (
-          <React.Fragment key={index}>
-            {message.content && isMe ? (
-              <div className={messageClassName}>
-                <p className="its-me-chat-box">{message.content}</p>
-              </div>
-            ) : (
-              <div>
-                {message.imageUrl && (
-                  <div className={messageClassName}>
-                    <div className="not-me-chat-message">
-                      <div>
-                        <img src={message.imageUrl} alt="프로필사진" />
-                      </div>
-                      <div>
-                        <p className="not-me-chat-name">{message.nickname}</p>
-                        <p>{message.content}</p>
+      <div className="private-chatroom-content-container">
+        {msgList.map((message: ChatMessage, index: number) => {
+          const isMe = message.nickname === userName;
+          const messageClassName = isMe ? "its-me-chat" : "not-me-chat";
+          const join = message.messageType === "JOIN";
+          return (
+            <React.Fragment key={index}>
+              {message.content && isMe ? (
+                <div className={messageClassName}>
+                  <p className="its-me-chat-box">{message.content}</p>
+                </div>
+              ) : (
+                <div>
+                  {message.imageUrl && (
+                    <div className={messageClassName}>
+                      <div className="not-me-chat-message">
+                        <div>
+                          <img src={message.imageUrl} alt="프로필사진" />
+                        </div>
+                        <div>
+                          <p className="not-me-chat-name">{message.nickname}</p>
+                          <p>{message.content}</p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-              </div>
-            )}
-            {join && (
-              <div className="in-out-message">
-                <p>{message.nickname}님이 참여하였습니다.</p>
-              </div>
-            )}
-          </React.Fragment>
-        );
-      })}
-      {/* </div> */}
+                  )}
+                </div>
+              )}
+              {join && (
+                <div className="in-out-message">
+                  <p>{message.nickname}님이 참여하였습니다.</p>
+                </div>
+              )}
+            </React.Fragment>
+          );
+        })}
+        {/* {isNewMessage && (
+          <button
+            type="button"
+            className="new-message-button"
+            onClick={handleButtonClick}
+          >
+            새로운 메시지가 도착했습니다. 최하단으로 이동
+          </button>
+        )} */}
+        <div ref={chatBottomRef}></div>
+      </div>
     </div>
   );
 }
