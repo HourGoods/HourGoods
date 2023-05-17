235d1ff3fa63cd489991512f551ba467911f7845
gimgyuyeon <8080claire@naver.com>
2023-05-17 15:29:30 +0900

    🚧 in progress : 경매장 UI 작업중

--- frontend/src/components/Auction/AuctionBox/index.tsx
+++ frontend/src/components/Auction/AuctionBox/index.tsx
@@ -37,7 +37,9 @@ export default function index({ bidList, inoutMsgList }: Props) {
   const dealInfo = location.state.dealinfo; // DealCard에 들어갈 Deal 정보
   const [nowBid, setNowBid] = useState(location.state.bidMoney); // 경매 시작가 혹은 입장하는 시점의 경매가
   const [nowCount, setNowCount] = useState(location.state.pplCnt); // 입장시점의 경매참여자 수
+  const [serverTime, setServerTime] = useState(""); // 입장시점의 경매참여자 수
   const dealId = location.state.dealid;
+  const [isDealCardClick, setIsDealCardClik] = useState(true);
 
   useEffect(() => {
     const fetchCurrentBid = async () => {
@@ -45,9 +47,9 @@ export default function index({ bidList, inoutMsgList }: Props) {
         const response = await AuctionAPI.getableAuction(dealId);
         console.log(response.data.result);
         const currentBid = response.data.result.currentBid;
-        const partCnt = response.data.result.participantCount;
+        const serverTime = response.data.result.currentTime;
         setNowBid(currentBid);
-        // setNowCount(partCnt);
+        setServerTime(serverTime);
       } catch (error) {
         console.error("Error fetching current bid:", error);
       }
@@ -55,9 +57,29 @@ export default function index({ bidList, inoutMsgList }: Props) {
     fetchCurrentBid();
   }, [nowBid, dealId]);
 
+  const dealCardClickHandler = () => {
+    setIsDealCardClik(!isDealCardClick);
+  };
+
   return (
     <div className="auctionbox-all-conatiner">
-      <AuctionDealCard dealInfo={dealInfo} />
+      {isDealCardClick ? (
+        <button
+          type="button"
+          className="dealCard-toggle-button"
+          onClick={dealCardClickHandler}
+        >
+          <p>▼ 거래 정보보기 ▼</p>
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="dealCard-toggle-button-active"
+          onClick={dealCardClickHandler}
+        >
+          <AuctionDealCard dealInfo={dealInfo} serverTime={serverTime} />
+        </button>
+      )}
       <RealtimeBidCard
         bidList={bidList}
         nowBid={nowBid}
