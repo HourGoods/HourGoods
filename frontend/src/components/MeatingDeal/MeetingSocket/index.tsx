d8dfa9605250215e9b7f4338d8dd0a6fa28fcb7a
gimgyuyeon <8080claire@naver.com>
2023-05-18 10:48:29 +0900

    ✨ feat : 소켓 종료로직 추가

--- frontend/src/components/MeatingDeal/MeetingSocket/index.tsx
+++ frontend/src/components/MeatingDeal/MeetingSocket/index.tsx
@@ -10,6 +10,7 @@ import Loading from "./Loading";
 
 // 소켓통신으로 수신받은 결과값을 저장한 인스턴스
 export interface MeetingDealInfo {
+  messageType: string; // '판매자정보'인지 '종료된정보'인지 판별하는 트리거
   otherNickname: string; // 판매자이름
   otherLongitude: number; // 판매자경도
   otherLatitude: number; // 판매자위도
@@ -32,6 +33,7 @@ export default function index({ tradeLocId }: Props) {
   // --------------------- 소켓 통신하기 -------------------------
   const clientRef = useRef<Client>();
   const [meetingInfo, setMeetingInfo] = useState<MeetingDealInfo>({
+    messageType: "", // 메세지타입
     otherNickname: "", // 판매자이름
     otherLongitude: 0, // 판매자경도
     otherLatitude: 0, // 판매자위도
@@ -77,9 +79,17 @@ export default function index({ tradeLocId }: Props) {
 
   // 소켓으로부터 받아오는 MeetingDealInfo 결과값
   const handleMessage = (message: string) => {
-    console.log("받아옵니다.");
     const parsedMessage = JSON.parse(message) as MeetingDealInfo;
-    setMeetingInfo(parsedMessage);
+    // 판매자정보를 받아옵니다.
+    if (parsedMessage.messageType === "Location") {
+      setMeetingInfo(parsedMessage);
+    }
+    // 종료된 정보이면 소켓연결 종료 후 리다이렉트
+    if (parsedMessage.messageType === "Done") {
+      disconnect();
+      alert("거래가 종료됩니다.");
+      navigate("/mypage");
+    }
   };
 
   return (
